9908f3ddae5c7cda115047751cb6c8d2b73a288b
Chayapat <jaroonsoft@hotmail.com>
2024-09-13 16:06:02 +0700

    weight_stable

--- renderer.js
+++ renderer.js
@@ -20,11 +20,11 @@ ipcRenderer.on("got-access-token", (event, accessToken) => {
 		let result = JSON.parse(grdetail)
 		$('#TM_Table_Main tbody').empty();
 		$('#TM_Table_Quantity tbody').empty();
-		
+
 		for (let i in result) {
 			let count = parseInt(i, 10) + 1
 			tb = '';
-			tb += '<tr id="' + (i + 1) + '"><td style="text-align: center">' + count + '.' + '</td><td>' + result[i].stcode + '</td><td>' + result[i].stname + '</td><td style="text-align: center">' + result[i].qty + '</td><td style="text-align: right">' + result[i].totalprice + '</td><td><button class="btn btn-primary" onclick="Show_Item(\'' + result[i].grcode + '\',\'' + result[i].stcode + '\',\'' + result[i].price + '\');"><i class="fas fa-sign-in-alt"></i> เลือก</button></td>';
+			tb += '<tr id="' + (i + 1) + '"><td style="text-align: center">' + count + '.' + '</td><td>' + result[i].stcode + '</td><td>' + result[i].stname + '</td><td style="text-align: center">' + result[i].qty + '</td><td><button class="btn btn-primary" onclick="Show_Item(\'' + result[i].grcode + '\',\'' + result[i].stcode + '\',\'' + result[i].price + '\',\'' + result[i].weight_stable + '\');"><i class="fas fa-sign-in-alt"></i> เลือก</button></td>';
 			tb += '</tr>';
 			$(tb).appendTo("#TM_Table_Main");
 
@@ -38,78 +38,96 @@ ipcRenderer.on("got-access-token", (event, accessToken) => {
 
 });
 
-function PrintBarcode(stcode, grcode, no,price,stname) {
+function PrintBarcode(stcode, grcode, no, price, stname) {
 	// alert(no)	
 	// alert($('#tx_unitweigt').val())
 	var inputweight = document.getElementById("tx_unitweigt");
-	if (inputweight.value > 0) {
-		$.post(
-			"https://tidmunzbuffet.com/api_app/barcode/add_barcode.php",
-			{
-				stcode: stcode,
-				stname: stname,
-				grcode: grcode,
-				unit_weight: $('#tx_unitweigt').val(),
-				no: no,
-				cost:price,
-			},
-			function (r2) {
-				// console.log(r2);
-				let response = JSON.parse(r2);
-				// console.log(response);
-				//   alert(response.barcode_id)
-				// ipc.send('message:Edit', data);
 
-				ipc.send('message:printtags', response);
+	if (weight_stable !== 'Y') {
 
-				Show_Item(response.grcode, response.stcode);
+		if (inputweight.value > 0) {
+			$.post(
+				"https://tidmunzbuffet.com/api_app/barcode/add_barcode.php",
+				{
+					stcode: stcode,
+					stname: stname,
+					grcode: grcode,
+					unit_weight: $('#tx_unitweigt').val(),
+					no: no,
+					cost: price,
+				},
+				function (r2) {
+					// console.log(r2);
+					let response = JSON.parse(r2);
+					// console.log(response);
+					//   alert(response.barcode_id)
+					// ipc.send('message:Edit', data);
 
-			}
-		).fail(function (error) {
+					ipc.send('message:printtags', response);
 
-			$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
-		});
-	} else {
+					Show_Item(response.grcode, response.stcode);
 
-		document.getElementById('txtresult').innerHTML = 'กรุณาชั่งสินค้าก่อน ยิง Barcode'
-		document.getElementById('txtresult').style.color = "red";
+				}
+			).fail(function (error) {
 
+				$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
+			});
+		} else {
+
+			document.getElementById('txtresult').innerHTML = 'กรุณาชั่งสินค้าก่อน ยิง Barcode'
+			document.getElementById('txtresult').style.color = "red";
+
+		}
+	}
+	else
+	{
+		alert('test')
 	}
 	// inputempcode.value = null;
 	// inputempcode.click();
 	event.preventDefault();
 }
 
-function Show_Item(grcode, stcode,price) {
+function Show_Item(grcode, stcode, price, weight_stable) {
+
+	$('#weight_stable').val(weight_stable);
 
 	$.post("https://tidmunzbuffet.com/api_app/barcode/getsup_barcode.php", { grcode: grcode, stcode: stcode }, function (grdetail) {
 		// console.log(grdetail);
 		let result = JSON.parse(grdetail)
-		
+
+		if (weight_stable == 'Y') {
+			$('#tx_noweight').show();
+			$('#tx_unitweigt').hide();
+		}
+		else {
+			$('#tx_noweight').hide();
+			$('#tx_unitweigt').show();
+		}
+
 		$('#TM_Table_Quantity tbody').empty();
 		for (let i in result) {
 			let barcode_status
 			let button_printf
-			if(result[i].barcode_status === "ยังไม่ออก barcode"){
-				barcode_status = '<td style="text-align: center;color : red;">'  + result[i].barcode_status + '</td>'
+			if (result[i].barcode_status === "ยังไม่ออก barcode") {
+				barcode_status = '<td style="text-align: center;color : red;">' + result[i].barcode_status + '</td>'
 				button_printf = '<td><button class="btn btn-success" onclick="PrintBarcode(\'' + result[i].stcode + '\',\'' + result[i].grcode + '\',\'' + result[i].no + '\',\'' + price + '\',\'' + result[i].stname + '\');"><i class="fa fa-print"></i> Print</button></td>'
-				}
-				else
-				{
-				barcode_status  = '<td style="text-align: center;color : secondary;">'  + result[i].barcode_status + '</td>'
+			}
+			else {
+				barcode_status = '<td style="text-align: center;color : secondary;">' + result[i].barcode_status + '</td>'
 				button_printf = '<td><button class="btn btn-secondary" onclick="PrintBarcode(\'' + result[i].stcode + '\',\'' + result[i].grcode + '\',\'' + result[i].no + '\',\'' + price + '\',\'' + result[i].stname + '\');"><i class="fa fa-print"></i> Print</button></td>'
 
 			}
 			tb = '';
-			tb += '<tr id="' + (i + 1) + '"><td  style="text-align: center;">' + result[i].no + '.' + '</td><td>' + result[i].stcode + '</td><td>' + result[i].stname + '</td><td  style="text-align: right;" >' + result[i].unit_weight + '</td>'+barcode_status+ button_printf;
+			tb += '<tr id="' + (i + 1) + '"><td  style="text-align: center;">' + result[i].no + '.' + '</td><td>' + result[i].stcode + '</td><td>' + result[i].stname + '</td><td  style="text-align: right;" >' + result[i].unit_weight + '</td>' + barcode_status + button_printf;
 			tb += '</tr>';
-			
-			
-				
+
+
+
 			$(tb).appendTo("#TM_Table_Quantity");
-			
+
 		}
-		
+
 	}).fail(function (error) {
 
 		$('#txtresult').text('อินเตอร์เน็ตมีปัญหา เชื่อมต่อไม่ได้')
